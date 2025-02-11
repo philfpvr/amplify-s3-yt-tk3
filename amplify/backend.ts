195d93ea1284599a8511f05fa1762536ee5841d5
philfpvr <phil@focalpointvr.com>
2025-02-11 13:48:40 +0000

    removed stroage from backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,18 +1,18 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { storage } from './storage/resource';
+
 
 const backend = defineBackend({
   auth,
   data,
-  storage
+  
 });
 
 
 backend.addOutput({
   storage: {
-    
-    bucket_name: "drm-vod-2"
+    aws_region: "eu-west-1",
+    bucket_name: "drm-vod-2",
   },
 });
