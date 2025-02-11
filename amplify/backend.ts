06d172fd9504b11629eb310dc95c4f8a635cc864
philfpvr <phil@focalpointvr.com>
2025-02-11 13:26:57 +0000

    added stroage line to backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { storage } from './storage/resource';
 
 const backend = defineBackend({
   auth,
   data,
+  storage
 });
 
 
